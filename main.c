ea9fb3cb6c3ea9e1cef700dd2cab8367a2fb9db0
Luana <luana.anselmo@ufv.br>
2025-01-19 21:49:02 -0300

    Primeiro commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "headers/casamento.h"
+
+int main() {
+    char filename[256];
+    char pattern[256];
+    int choice;
+
+    printf("Inclua o nome do arquivo: ");
+    scanf("%255s", filename);
+    printf("Digite o padrão que deseja encontrar: ");
+    scanf("%255s", pattern);
+
+    printf("Escolha o algoritmo desejado:\n");
+    printf("1. Shift-And\n");
+    printf("2. KMP\n");
+    printf("Digite sua escolha: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            search_from_file(filename, pattern, shift_and);
+            break;
+        case 2:
+            search_from_file(filename, pattern, kmp_search);
+            break;
+        default:
+            printf("Escolha inválida.\n");
+    }
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+all: main.c sources/casamento.c 
+	gcc -o programa main.c sources/casamento.c 
